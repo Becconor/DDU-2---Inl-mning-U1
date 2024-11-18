bb6b53ce92b0320063115c56ca07f2f57cbdc728
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2024-11-18 11:57:14 +0100

    Skapat första for loopen och if-en

--- index.js
+++ index.js
@@ -11,18 +11,26 @@
 
 // Recommended: constants with references to existing HTML-elements
 
-
-
-
-
-
-
-
-
-
 // Recommended: Ask for the city name and then the rest of the code
+const titleHead = document.querySelector("title");
+const cityTitle = document.querySelector("h2");
+const cityUndertitle = document.querySelector("h3");
 
 
+const enterCity = prompt("Vilken stad?");
+let cityWasFound = false;
 
+for (let citiesKey in cities) {
+    if (enterCity == cities[citiesKey].name) {
+        cityWasFound = true;
+        cityTitle.innerHTML = `${enterCity} (${cities[citiesKey].country})`;
+        titleHead.innerHTML = enterCity;
+        break; // Loopen tar slut när stad hittats eller inte finns.
+    } 
+}
 
-
+if (cityWasFound == false) {
+    cityTitle.innerHTML = `${enterCity} finns inte i databasen`;
+    cityUndertitle.innerHTML = " ";
+    titleHead.innerHTML = "Not found";
+}
