e69d960a2e958989545f6a09be4128e487d6c77d
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2024-11-20 14:05:45 +0100

    Skapad tabell
    Skapat distanstabell

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const titleHead = document.querySelector("title");
 const cityTitle = document.querySelector("h2");
 const cityUndertitle = document.querySelector("h3");
+const cityBoxes = document.querySelector("#cities");
+const distanceTable = document.querySelector("#table");
 
 let enterCity = prompt("Vilken stad?")
 
@@ -24,4 +26,50 @@ if (cityWasFound == false) {
     cityTitle.innerHTML = `${enterCity} finns inte i databasen`;
     cityUndertitle.innerHTML = " ";
     titleHead.innerHTML = "Not found";
+}
+
+
+for (let i = 0; i <= 39; i++) {
+    for (let j = 0; j <= 39; j++) {
+        let gridCell = document.createElement("div"); 
+        gridCell.classList.add("cell"); 
+        distanceTable.appendChild(gridCell); 
+
+        if (i >= 1 && j >= 1) {
+            for (let distanceKey in distances) {
+                if (distances[distanceKey].city1 == j - 1 && distances[distanceKey].city2 == i - 1) {
+                    gridCell.textContent = distances[distanceKey].distance / 10; 
+                }
+                if (distances[distanceKey].city2 == j - 1 && distances[distanceKey].city1 == i - 1) {
+                    gridCell.textContent = distances[distanceKey].distance / 10; 
+                }
+            }
+        }
+
+        if (i == 0) {
+            gridCell.classList.add("head_row"); 
+            gridCell.textContent = j - 1; 
+        }
+
+        if (j == 0) {
+            gridCell.classList.add("head_column"); 
+            gridCell.textContent = i - 1; 
+        }
+
+        if (i == j) {
+            gridCell.textContent = " ";
+        }
+
+        if (i % 2 == 1) {
+            gridCell.classList.add("even_row"); 
+        }
+
+        if (j % 2 == 1 && i != 0) {
+            gridCell.classList.add("even_col"); 
+        }
+
+        if (j == 0 && i >= 1) {
+            gridCell.textContent = cities[i - 1].id + " - " + cities[i - 1].name;
+        }
+    }
 }
