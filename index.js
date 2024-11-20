93a52c0f8d93eef12553f6d9aff829b7829c24e3
Rebecca Sandberg <rebeccasandberg02@gmail.com>
2024-11-20 13:58:54 +0100

    Ändra namn på webbsidans huvudrubrik
    Skapat variabler för att användaren ska kunna skriva in en stad och den ska sökas efter i databasen. Finns den skrivs den ut. Finns den inte skrivs det att den inte finns.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // Recommended: All functions declared here
+const titleHead = document.querySelector("title");
+const cityTitle = document.querySelector("h2");
+const cityUndertitle = document.querySelector("h3");
+
+let enterCity = prompt("Vilken stad?")
+
+
 
 // Recommended: constants with references to existing HTML-elements
 
+
 // Recommended: Ask for the city name and then the rest of the code
+for (let keyCity in cities) {
+    if (enterCity == cities[keyCity].name) {
+        cityWasFound = true;
+        cityTitle.innerHTML = `${enterCity} (${cities[keyCity].country})`;
+        titleHead.innerHTML = enterCity;
+        break;
+    }
+}
+
+if (cityWasFound == false) {
+    cityTitle.innerHTML = `${enterCity} finns inte i databasen`;
+    cityUndertitle.innerHTML = " ";
+    titleHead.innerHTML = "Not found";
+}
